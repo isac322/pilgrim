e143d2ced56389d87abbef1272f02dafac81b94f
Isac Byeonghoon Yoo <isac.yoo@buzzvil.com>
2021-03-03 16:34:00 +0900

    Remove doc url

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from starlette.templating import Jinja2Templates
 
 from models import QuizForm
 
-app = FastAPI()
+app = FastAPI(title='Pilgrim', docs_url=None, redoc_url=None)
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
