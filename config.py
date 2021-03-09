a10f5443732d023e43f6d285d758894b599682c3
Isac Byeonghoon Yoo <bh322yoo@gmail.com>
2021-03-10 00:38:23 +0900

    Change base img (#5)
    * Use isac322/uvicorn as base image
    
    * Set maintainer of docker image
    
    * Optimize image build
    
    * Fix db url

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     qna_file_path: FilePath = 'resource/qna.json'
     grade_names_file_path: FilePath = 'resource/grade_names.json'
     cafe_names_file_path: FilePath = 'resource/cafe_names.json'
-    db_url: str = 'db.sqlite3'
+    db_url: str = 'sqlite://db.sqlite3'
 
     class Config:
         env_prefix = 'pilgrim_'
